c9e5334d537c36f8c3e0823a7bd6e0be7a679793
Thomas Sjöholm <ThompZon@gmail.com>
2014-09-26 11:12:40 +0200

    added first problem

---
+++ lab1/Knapsack.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package popup.lab1;
+
+/**
+ * @author ThompZon
+ *
+ */
+public class Knapsack {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
