5821f032d8ebde6d1a862476b1dbf632e0348b54
Thomas Sjöholm <ThompZon@gmail.com>
2014-10-17 10:55:59 +0200

    setup lab2

---
+++ lab2/Dijkstras.java
@@ -0,0 +1,17 @@
+package popup.lab2;
+
+/**
+ * @author ThompZon
+ *
+ */
+public class Dijkstras {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
