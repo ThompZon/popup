523498dbc6c158bb243967351ce4a271a0e8343d
Thomas Sjöholm <ThompZon@gmail.com>
2014-10-19 19:13:10 +0200

    tried to optimize, but kattis still fail

--- lab2/FordFulkerson.java
+++ lab2/FordFulkerson.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 
 
 /**
@@ -19,164 +20,120 @@ import java.util.Set;
 public class FordFulkerson {
 	public static final int NOPARENT = -1;
 	public static final boolean DEBUGGING = false;
-	
-	public static class Node{
-		public Map<Integer, Edge> neigbors;
-		public int index;
-		public int parent;
-		public boolean visited;
-		
-		public Node(int i){
-			this.neigbors = new HashMap<Integer, Edge>();
-			this.index = i;
-			this.visited = false;
-			this.parent = NOPARENT;
-		}
-	}
-	
-	public static class Edge {
+
+	public static class Edge{
 		public int from;
 		public int to;
 		
 		public int capacity;
 		public int flow;
-
-		/**
-		 * Initialize an edge This connects two nodes
-		 *
-		 * @param from the starting point of this edge
-		 * @param to the destination of this edge
-		 * @param capacity the distance or cost for this edge
-		 */
-		public Edge(int from, int to, int capacity) {
+		public int restcapacity;
+		public Edge(int from, int to, int capacity){
 			this.from = from;
 			this.to = to;
 			this.capacity = capacity;
+			this.restcapacity = capacity;
 			this.flow = 0;
 		}
 	}
-	
+
 	private Set<Edge> edgesUsed;
-	private Node[] nodes;
+	
+	private Map<Integer, Edge>[] neighborList;
+	
 	private int source;
 	private int sink;
 	private int maxFlow;
-	
-	public FordFulkerson(int nodesNumberOf){
-		this.nodes = new Node[nodesNumberOf];
-	}
-	
-	public FordFulkerson(Node[] nodeArray, int source, int sink){
-		this.nodes = nodeArray;
+	private int nodesNumberOf;
+
+	public FordFulkerson(Map<Integer, Edge>[] neighLists, int source, int sink, int nodes){
+		this.neighborList = neighLists;
 		this.source = source;
 		this.sink = sink;
 		this.edgesUsed = new HashSet<Edge>();
+		this.nodesNumberOf = nodes;
 	}
-	
-	public void setSource(int nodeIndex){
-		this.source = nodeIndex;
-	}
-	
-	public void setSink(int nodeIndex){
-		this.sink = nodeIndex;
-	}
-	
-	public void setNode(int nodeIndex, Node newNode){
-		this.nodes[nodeIndex] = newNode;
-	}
-	
-	public void addEdge(int from, int to, int capacity){
-		this.nodes[from].neigbors.put(to, new Edge(from, to, capacity));
-		this.nodes[to].neigbors.put(from, new Edge(to, from, 0));
-	}
-	
+
 	public int getMaxFlow(){
 		return maxFlow;
 	}
-	
+
 	public Set<Edge> getEdgesUsed(){
 		return edgesUsed;
 	}
-	
-	public Node[] getNodes(){
-		return nodes;
-	}
-	
+
 	public void runAlgorithm() {
-		ArrayDeque<Node> queue = new ArrayDeque<Node>();
+		ArrayDeque<Integer> queue = new ArrayDeque<Integer>();
+		boolean[] visitedList = new boolean[nodesNumberOf];
+		int[] parentList = new int[nodesNumberOf];
+		
 		this.maxFlow = 0;
 		while (true) 
 		{
 			//Start with source:
-			queue.push(this.nodes[this.source]);
-			for(short i = 0; i < nodes.length; i++)
+			queue.push(this.source);
+			for(short i = 0; i < nodesNumberOf; i++)
 			{
 				//Reset path to find additional paths:
-				nodes[i].visited = false;
-				nodes[i].parent = NOPARENT;
+				visitedList[i] = false;
+				parentList[i] = NOPARENT;
 			}
 			//Visited start:
-			nodes[this.source].visited = true;
+			visitedList[this.source] = true;
 			//DFS from source to sink
 			while (queue.isEmpty() == false)
 			{
-				Node current = queue.pop();
-				for(Edge e : current.neigbors.values())
+				int current = queue.pop();
+				for(Edge edge : neighborList[current].values())
 				{
-					if(nodes[e.to].visited == false && e.capacity - e.flow > 0)
+					if(visitedList[edge.to] == false && edge.restcapacity > 0)
 					{
-						//if we have capacity left on this edge
-						queue.push(nodes[e.to]);
-						nodes[e.to].visited = true;
-						nodes[e.to].parent = e.from;
-						if(e.to == this.sink)
+						parentList[edge.to] = edge.from;
+						if(edge.to == this.sink)
 						{
 							//found sink, done for this round!
 							queue.clear();
 							break;
 						}
+						//if we have capacity left on this edge
+						queue.push(edge.to);
+						visitedList[edge.to] = true;
 					}
 				}
 			}
-			
-			//we compute the path maxCapacity
-			//where = target
+
 			int to = sink;
-			if(nodes[to].parent == NOPARENT)
+			if(parentList[to] == NOPARENT)
 			{
 				if(DEBUGGING)System.err.println("NO PATH FOUND");
 				//If we failed to find path, we are finished here!
-				break;
+				return;
 			} 
 			else {
+				//we compute the path maxCapacity
 				int pathCapacity = Integer.MAX_VALUE;
 				if(DEBUGGING)System.err.print("\n Backtracking: ");
-				while (nodes[to].parent != NOPARENT) 
+				while (parentList[to] != NOPARENT) 
 				{
 					if(DEBUGGING)System.err.print(to + ", ");
 					//Backtrack to see how much flow that this path can take
-					Node from = nodes[nodes[to].parent]; 
-					Edge e = from.neigbors.get(to);
-					pathCapacity = Math.min(pathCapacity, e.capacity - e.flow);
-					to = from.index;
+					int from = parentList[to]; 
+					Edge e = neighborList[from].get(to);
+					pathCapacity = Math.min(pathCapacity, e.restcapacity);
+					to = from;
 				}
 				if(DEBUGGING)System.err.println("\n");
 
 				//Backtrack to set the flow to the maximum flow this path can take
 				to = sink;
-				while (nodes[to].parent != NOPARENT) 
+				while (parentList[to] != NOPARENT) 
 				{
-					Node from = nodes[nodes[to].parent]; 
-					Edge e = from.neigbors.get(to);
-					Edge rE = nodes[to].neigbors.get(from.index);
+					int from = parentList[to];
 					
-					
+					Edge e = neighborList[from].get(to);
 					e.flow += pathCapacity;
-					rE.flow = -e.flow;
-					
+					e.restcapacity = e.capacity - e.flow;
 					if(DEBUGGING)System.err.println("EDGE - from: " + e.from + ", to: " + e.to + ", flow: " + e.flow);
-					if(DEBUGGING)System.err.println("BACKWARDS EDGE - from: " + rE.from + ", to: " + rE.to + ", flow: " + rE.flow);
-
 					//store used edges:
 					if(e.flow > 0){
 						if(DEBUGGING)System.err.println("EDGE IN SOLUTION - from: " + e.from + ", to: " + e.to + ", flow: " + e.flow);
@@ -185,6 +142,12 @@ public class FordFulkerson {
 						if(DEBUGGING)System.err.println("EDGE DELETED - from: " + e.from + ", to: " + e.to + ", flow: " + e.flow);
 						edgesUsed.remove(e);
 					}
+
+					Edge rE = neighborList[to].get(from);
+					rE.flow -= pathCapacity;
+					rE.restcapacity = rE.capacity - rE.flow;
+					if(DEBUGGING)System.err.println("BACKWARDS EDGE - from: " + rE.from + ", to: " + rE.to + ", flow: " + rE.flow);
+					//store used back edges:
 					if(rE.flow > 0){
 						if(DEBUGGING)System.err.println("BACK EDGE IN SOLUTION - from: " + rE.from + ", to: " + rE.to + ", flow: " + rE.flow);
 						edgesUsed.add(rE);
@@ -192,49 +155,49 @@ public class FordFulkerson {
 						if(DEBUGGING)System.err.println("BACK DELETED - from: " + rE.from + ", to: " + rE.to + ", flow: " + rE.flow);
 						edgesUsed.remove(rE);
 					}
-					
 					//Go to previous node:
-					to = from.index;
+					to = from;
 				}
 				//Update maxflow
 				this.maxFlow += pathCapacity;
 			}
 		}
 	}
-	
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		Kattio kio = new Kattio(System.in);
-		
+
 		//Intput:
 		int nodeNumberOf = kio.getInt();
 		int edgeNumberOf = kio.getInt();
 		int source = kio.getInt();
 		int sink = kio.getInt();
-		Node[] nodes = new Node[nodeNumberOf];
+		@SuppressWarnings("unchecked")
+		Map<Integer, Edge>[] list = new Map[nodeNumberOf];
 		for(int i = 0; i < nodeNumberOf; i++){
-			nodes[i] = new Node(i);
+			list[i] = new TreeMap<Integer, Edge>();
 		}
 		for(int i = 0; i < edgeNumberOf; i++){
 			int from = kio.getInt();
 			int to = kio.getInt();
 			int capacity = kio.getInt();
 			//forwards edge:
-			nodes[from].neigbors.put(to, new Edge(from, to, capacity));
+			list[from].put(to, new Edge(from, to, capacity));
 			//Backwards edge:
-			nodes[to].neigbors.put(from, new Edge(to, from, 0));
+			list[to].put(from, new Edge(to, from, 0));
 		}
-		
+
 		//Run Algorithm:
-		FordFulkerson ff = new FordFulkerson(nodes, source, sink);
+		FordFulkerson ff = new FordFulkerson(list, source, sink, nodeNumberOf);
 		ff.runAlgorithm();
-		
+
 		//Output:
 		int maxFlow = ff.getMaxFlow();
 		Set<Edge> used = ff.getEdgesUsed();
-		
+
 		StringBuilder sb = new StringBuilder();
 		sb.append(nodeNumberOf);
 		sb.append(' ');
@@ -251,7 +214,7 @@ public class FordFulkerson {
 			sb.append(e.flow);
 		}
 		kio.println(sb.toString());
-		
+
 		//Close:
 		kio.flush();
 		kio.close();
